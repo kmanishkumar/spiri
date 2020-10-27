e42bd77c04070c3f60c8b9f9e42f93b5fcc28d6e
@jkarthikeyan1 <jkarthikeyan@zibtek.in>
2020-10-27 21:54:20 +0530

    added git ignore

--- lib/features/dashboard/dashboard.dart
+++ lib/features/dashboard/dashboard.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
 import 'dart:ui';
 
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
